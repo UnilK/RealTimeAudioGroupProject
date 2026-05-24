cf4a6692665f6d33aff89d075827f6527d01c19a
Olli Pauna <olli.p.pauna@protonmail.com>
2026-05-24 19:20:43 +0300

    add filter stuff

--- projects/OurRealTimeAudioApp/PluginProcessor.cpp
+++ projects/OurRealTimeAudioApp/PluginProcessor.cpp
@@ -7,9 +7,21 @@
 #include "math/constants.h"
 #include "math/fft.h"
 
+
+void modeMix(float mode, float& lpf, float& bpf, float& hpf)
+{
+    mode = std::clamp(mode, -1.f, 1.f);
+    lpf = std::fmax(-mode, 0.f);
+    bpf = std::fmax(1.f - std::fabs(mode), 0.f);
+    hpf = std::fmax(mode, 0.f);
+}
+
 static const std::vector<mrta::ParameterInfo> ParameterInfos
 {
     { Param::ID::PostGain,  Param::Name::PostGain,  "dB", 0.0f, -60.f, 12.f, 0.1f, 3.8018f },
+    { Param::ID::Freq, Param::Name::Freq, Param::Unit::Hz, 500.0f, Param::Ranges::FreqMin, Param::Ranges::FreqMax, Param::Ranges::FreqInc, Param::Ranges::FreqSkw },
+    { Param::ID::Reso, Param::Name::Reso, "", 1.0f, Param::Ranges::ResoMin, Param::Ranges::ResoMax, Param::Ranges::ResoInc, Param::Ranges::ResoSkw },
+    { Param::ID::Mode, Param::Name::Mode, "", 0.0f, Param::Ranges::ModeMin, Param::Ranges::ModeMax, Param::Ranges::ModeInc, Param::Ranges::ModeSkw}
 };
 
 MainProcessor::MainProcessor() :
@@ -28,6 +40,31 @@ MainProcessor::MainProcessor() :
             gain = dbValue;
 
         });
+
+    registerParameterCallback(Param::ID::Freq,
+        [this] (float value, bool force)
+        {
+            freqHz = value;
+            freqRamp.setTarget(value, force);
+        });
+
+    registerParameterCallback(Param::ID::Reso,
+        [this] (float value, bool force)
+        {
+            reso = value;
+            resoRamp.setTarget(value, force);
+        });
+
+    registerParameterCallback(Param::ID::Mode,
+        [this] (float value, bool force)
+        {
+            mode = value;
+            float lpf(0.f), bpf(0.f), hpf(0.f);
+            modeMix(mode, lpf, bpf, hpf);
+            lpfRamp.setTarget(lpf, force);
+            bpfRamp.setTarget(bpf, force);
+            hpfRamp.setTarget(hpf, force);
+        });   
 }
 
 MainProcessor::~MainProcessor()
@@ -35,15 +72,31 @@ MainProcessor::~MainProcessor()
 }
 
 void MainProcessor::prepare(double sampleRate, int samplesPerBlock)
-{
+{   
     juce::uint32 numChannels { static_cast<juce::uint32>(std::max(getMainBusNumInputChannels(), getMainBusNumOutputChannels())) };
     
     pitchDetector.prepare({ .framerate = (float)sampleRate });
+
 
     int radius = pitchDetector.get_reguired_buffer_radius()+ 1;
     
     ibuff.resize(radius * 2, 0.0f);
     ibuff.set_offset(radius);
+
+
+    svf.prepare(sampleRate);
+
+    float lpf, bpf, hpf;
+    modeMix(mode, lpf, bpf, hpf);
+    lpfRamp.prepare(sampleRate, true, lpf);
+    bpfRamp.prepare(sampleRate, true, bpf);
+    hpfRamp.prepare(sampleRate, true, hpf);
+
+    freqInBuffer.setSize(1, samplesPerBlock);
+    resoInBuffer.setSize(1, samplesPerBlock);
+    lpfOutBuffer.setSize(2, samplesPerBlock);
+    bpfOutBuffer.setSize(2, samplesPerBlock);
+    hpfOutBuffer.setSize(2, samplesPerBlock);
 }
 
 void MainProcessor::process(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& /*midiMessages*/)
@@ -68,6 +121,36 @@ void MainProcessor::process(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& 
 
         for(int j=0; j<m; j++) buffer.getWritePointer(j)[i] = sample;
     }
+
+    freqInBuffer.clear();
+    resoInBuffer.clear();
+    lpfOutBuffer.clear();
+    bpfOutBuffer.clear();
+    hpfOutBuffer.clear();
+
+    freqRamp.applySum(freqInBuffer.getWritePointer(0), n);
+    resoRamp.applySum(resoInBuffer.getWritePointer(0), n);
+
+    svf.process(lpfOutBuffer.getWritePointer(0),
+                    bpfOutBuffer.getWritePointer(0),
+                    hpfOutBuffer.getWritePointer(0),
+                    buffer.getReadPointer(0),
+                    freqInBuffer.getReadPointer(0),
+                    resoInBuffer.getReadPointer(0),
+                    n);
+
+    lpfRamp.applyGain(lpfOutBuffer.getArrayOfWritePointers(), m, n);
+    bpfRamp.applyGain(bpfOutBuffer.getArrayOfWritePointers(), m, n);
+    hpfRamp.applyGain(hpfOutBuffer.getArrayOfWritePointers(), m, n);
+    buffer.clear();
+
+    for (int ch = 0; ch < m; ++ch)
+    {
+        buffer.addFrom(ch, 0, lpfOutBuffer, ch, 0, n);
+        buffer.addFrom(ch, 0, bpfOutBuffer, ch, 0, n);
+        buffer.addFrom(ch, 0, hpfOutBuffer, ch, 0, n);
+    }
+
 }
 
 juce::AudioProcessorEditor* MainProcessor::createEditor()
